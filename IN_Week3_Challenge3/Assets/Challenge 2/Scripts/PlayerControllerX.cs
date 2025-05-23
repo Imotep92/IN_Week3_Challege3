b3d2ead299e3ae663a095011cfe8d9052109a45b
Imotep92 <Imogyn.nicholas@yahoo.co.uk>
2025-05-23 11:54:55 +0100

    1st commit, 23/05/25 11:50am
    Week three debugging challenge
    - dogs spawn when player presses space bar
    - balls spawn top of screen at random time intervals
    - dog spawning is on cooldown
    -added array to select random balls

---
+++ IN_Week3_Challenge3/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public bool isCoolDown;
+    private float coolDown = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isCoolDown == false)
+            {
+                SpawnDog();
+                StartCoroutine(CoolDown());
+            }
+        }
+    }
+        IEnumerator CoolDown( )
+    {
+        isCoolDown = true;
+        yield return new WaitForSeconds(coolDown);
+        isCoolDown = false;
+    }
+    private void SpawnDog()
+    {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+    }
+               
+}
